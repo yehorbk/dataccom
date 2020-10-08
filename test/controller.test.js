c9ff8501aa64c1b11abd72ca786e98088e72da65
Yehor Bublyk <yehor.bk@gmail.com>
2020-10-08 17:42:26 +0300

    tests with promise functions added to controller

--- test/controller.test.js
+++ test/controller.test.js
@@ -32,4 +32,35 @@ mocha.describe('Controller test', () => {
     const expected = { name: 'John', position: 'Developer' };
     assert.deepStrictEqual(result, expected);
   });
+
+  mocha.it('Chain of promise functions without scheme', async () => {
+    const callback = (name, value) =>
+      new Promise(resolve => resolve({ [name]: value }));
+    const result = await controller
+      .chain()
+      .fetch(callback, 'name', 'John')
+      .fetch(callback, 'age', 27)
+      .fetch(callback, 'position', 'Developer')
+      .collect();
+    const expected = { name: 'John', age: 27, position: 'Developer' };
+    assert.deepStrictEqual(result, expected);
+  });
+
+  mocha.it('Chain of promise functions with scheme', async () => {
+    const callback = (name, value) =>
+      new Promise(resolve => resolve({ [name]: value }));
+    const scheme = {
+      name: String,
+      position: String,
+    };
+    const result = await controller
+      .chain()
+      .fetch(callback, 'name', 'John')
+      .fetch(callback, 'age', 27)
+      .fetch(callback, 'position', 'Developer')
+      .apply(scheme)
+      .collect();
+    const expected = { name: 'John', position: 'Developer' };
+    assert.deepStrictEqual(result, expected);
+  });
 });
