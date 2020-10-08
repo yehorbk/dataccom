248ed149de0f3f8b04cf7f7af4d7db243be659d9
Yehor Bublyk <yehor.bk@gmail.com>
2020-10-08 17:05:43 +0300

    collector implemented

---
+++ test/collector.test.js
@@ -0,0 +1,18 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+const Executor = require('../lib/collector');
+
+mocha.describe('Collector test', () => {
+  mocha.it('Chain of simple functions', () => {
+    const callback = (name, value) => () => ({ [name]: value });
+    const executor = new Executor();
+    executor.collect(callback('name', 'John'));
+    executor.collect(callback('age', 2));
+    executor.collect(callback('position', 'Developer'));
+    const expected = { name: 'John', age: 2, position: 'Developer' };
+    executor.run(data => {
+      assert.deepStrictEqual(data, expected);
+    });
+  });
+});
