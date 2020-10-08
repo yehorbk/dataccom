3bf22a59b1bafdacf67afe6ee1a182880a73ebb2
Yehor Bublyk <yehor.bk@gmail.com>
2020-10-08 17:11:30 +0300

    test for promise functions added to collector

--- test/collector.test.js
+++ test/collector.test.js
@@ -15,4 +15,17 @@ mocha.describe('Collector test', () => {
       assert.deepStrictEqual(data, expected);
     });
   });
+
+  mocha.it('Chain of promise functions', () => {
+    const callback = (name, value) => () =>
+      new Promise(resolve => resolve({ [name]: value }));
+    const executor = new Executor();
+    executor.collect(callback('name', 'John'));
+    executor.collect(callback('age', 2));
+    executor.collect(callback('position', 'Developer'));
+    const expected = { name: 'John', age: 2, position: 'Developer' };
+    executor.run(data => {
+      assert.deepStrictEqual(data, expected);
+    });
+  });
 });
